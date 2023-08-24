22e71575d71138cd518ecd2f13ef1680a32c2d85
Andre <abrizidobasilio@gmail.com>
2023-08-24 10:59:50 -0300

    ultimo commit

--- API/PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ API/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -4,10 +4,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
+//Adiciona o serviço Swagger
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//Começa a configuração do Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+//Finaliza a configuração Swagger
+
 
 //Adiciona mapeamento dos Controllers
 app.MapControllers();
