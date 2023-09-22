ccee0e7b6ef0b9f218e6f5917590270998c0a408
Andre <abrizidobasilio@gmail.com>
2023-09-22 08:25:09 -0300

    tipoeEvento repository acabado

--- API - Eventplus/apiweb.eventplus.manha/Controllers/InstituicaoController.cs
+++ API - Eventplus/apiweb.eventplus.manha/Controllers/InstituicaoController.cs
@@ -1,4 +1,5 @@
-﻿using apiweb.eventplus.manha.Interfaces;
+﻿using apiweb.eventplus.manha.Domains;
+using apiweb.eventplus.manha.Interfaces;
 using apiweb.eventplus.manha.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,26 @@ namespace apiweb.eventplus.manha.Controllers
         public InstituicaoController()
         {
             _instituicaoRepository = new InstituicaoRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Instituicao instituicao)
+        {
+            try
+            {
+                if (instituicao != null)
+                {
+                    _instituicaoRepository.Cadastrar(instituicao);
+                    return Ok("Usuario cadastrado!");
+
+                }
+
+                return Ok("Usuario não foi inserido corretamente!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
---
+++ API - Eventplus/apiweb.eventplus.manha/Controllers/TipoEventoController.cs
@@ -0,0 +1,20 @@
+﻿using apiweb.eventplus.manha.Interfaces;
+using apiweb.eventplus.manha.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiweb.eventplus.manha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class TipoEventoController : ControllerBase
+    {
+        private ITipoEventoRepository _tipoEventoRepository;
+
+        public TipoEventoController()
+        {
+            _tipoEventoRepository = new TipoEventoRepository();
+        }
+    }
+}
